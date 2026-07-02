85f80226e537da519edf3921bc7d29af6e68963d
Javed Khan <contact@digitaldreams.in>
2026-07-02 18:02:28 +0530

    gemini api key added

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 from dotenv import load_dotenv
 import os
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
--- services/ai_service.py
+++ services/ai_service.py
@@ -1,33 +1,16 @@
-from openai import OpenAI
-from openai import APIStatusError
-from openai import RateLimitError
-
-from config import OPENAI_API_KEY
+import google.generativeai as genai
+from config import GEMINI_API_KEY
 
 
 class AiService:
 
     def __init__(self):
-        self.client = OpenAI(api_key=OPENAI_API_KEY)
+        genai.configure(api_key=GEMINI_API_KEY)
+        self.client = genai
 
     def chat(self, message: str):
-        try:
-            response = self.client.chat.completions.create(
-                model='gpt-4.1-mini',
-                messages=[
-                    {
-                        "role": "system",
-                        "content": "You are a helpful AI Assistant"
-                    },
-                    {
-                        "role": "user",
-                        "content": message
-                    }
-                ]
-            )
-            return response.choices[0].message.content
-        except (RateLimitError, APIStatusError) as exc:
-            return (
-                "The AI service is temporarily unavailable due to a quota or rate-limit issue. "
-                "Please try again later."
-            )
+        
+            model = genai.GenerativeModel('gemini-2.5-flash')
+            response = model.generate_content(message)
+            return response.text
+        
